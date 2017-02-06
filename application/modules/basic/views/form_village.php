c7235b39eb28a9d8a399707d074a606169ed12cf
vthawat <thawat.va@email.psu.ac.th>
2017-02-06 21:18:05 +0700

    add form_village

---
+++ application/modules/basic/views/form_village.php
@@ -0,0 +1,22 @@
+<form method="post" class="form-horizontal" action="<?php if(!empty($action)) print $action?>">
+	<div class="form-group">
+    <label for="ministry" class="col-sm-2 control-label">ชื่อหมู่บ้าน*</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" name="MINISTRY_NAME" id="ministry" placeholder="ชื่อหมู่บ้าน" value="<?php if(!empty($edit_item)) print $edit_item->MINISTRY_NAME?>">
+    </div>
+  </div>
+  <div class="form-group">
+   <label for="ministry" class="col-sm-2 control-label">เลือกจังหวัด*</label>
+   <div class="col-sm-10">
+   <select class="form-control provice_id">
+   </select>
+   </div>
+  </div>
+  <?php if(!empty($action_btn)):?>
+  	<div class="form-group">
+    <div class="col-sm-10 col-sm-offset-2">
+      <?=$action_btn?>
+    </div>
+  </div>
+  <?php endif;?>
+</form>
