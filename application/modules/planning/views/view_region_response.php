a1f2e301bda107b61b0da9e0ca79944756ec4b80
vthawat <thawat.va@email.psu.ac.th>
2017-02-20 21:41:10 +0700

    update response_household

--- application/modules/planning/views/view_region_response.php
+++ application/modules/planning/views/view_region_response.php
@@ -9,15 +9,16 @@
             <tr>
                 <th colspan="2" class="text-center">พื้นที่ลงโครงการ</th>
             </tr>
-	    	<tr>
+			<tr>
 				<th class="bg-gray text-right col-lg-4">ปีงบประมาณ</th>
 				<td><?=$project_planning->YEAR?></td>
-			</tr>
+			</tr>            
 			<tr>
 				<th class="bg-gray text-right col-lg-4">จังหวัด</th>
 				<td><?=$project_planning->PROVINCE_NAME?></td>
 			</tr>
-                        <tr>
+            
+            <tr>
 				<th class="bg-gray-active text-right col-lg-4">อำเภอ</th>
 				<td><?=$project_planning->AMPHUR_NAME?></td>
 			</tr>
@@ -29,13 +30,40 @@
                 <th colspan="2" class="text-center">ความต้องการในระดับครัวเรือน</th>
             </tr>
             <tr>
-				<th class="bg-success text-right col-lg-4">จำนวนหมู่บ้านทั้งหมด</th>
+				<th class="bg-danger text-right col-lg-4">จำนวนหมู่บ้านทั้งหมด</th>
 				<td><h2 class="text-blue"><?=$require_household->count_village_by_district_id($project_planning->DISTRICT_ID)?></h2>หมู่บ้าน</td>
 			</tr>
                         <tr>
-				<th class="bg-success text-right col-lg-4">จำนวนครัวเรือนทั้งหมด</th>
+				<th class="bg-danger text-right col-lg-4">จำนวนครัวเรือนทั้งหมด</th>
 				<td><h2 class="text-blue"><?=$require_household->count_household_by_district_id($project_planning->DISTRICT_ID)?></h2>ครัวเรือน</td>
 			</tr>
+            <tr>
+				<th class="bg-danger text-right">ความเดือดร้อน</th>
+				<td>
+                    <ul class="list-group">
+                        <li class="list-group-item">รายได้<span class="pull-right"><span class="badge bg-red"><?=$require_household->count_affliction_by_district_id('AFFLICTION_INCOME',$project_planning->DISTRICT_ID)?></span> ครัวเรือน</span></li>
+                        <li class="list-group-item">ความเจ็บป่วย<span class="pull-right"><span class="badge bg-red"><?=$require_household->count_affliction_by_district_id('AFFLICTION_AILING',$project_planning->DISTRICT_ID)?></span> ครัวเรือน</span></li>
+                        <li class="list-group-item">ที่อยู่อาศัย<span class="pull-right"><span class="badge bg-red"><?=$require_household->count_affliction_by_district_id('AFFLICTION_HOUSE',$project_planning->DISTRICT_ID)?></span> ครัวเรือน</span></li>
+                        <li class="list-group-item">ความปลอดภัย<span class="pull-right"><span class="badge bg-red"><?=$require_household->count_affliction_by_district_id('AFFLICTION_SAFETY',$project_planning->DISTRICT_ID)?></span> ครัวเรือน</span></li>
+                        <li class="list-group-item">อื่นๆ<span class="pull-right"><span class="badge bg-red"><?=$require_household->count_affliction_by_district_id('AFFLICTION_ETC',$project_planning->DISTRICT_ID)?></span> ครัวเรือน</span></li>
+                    </ul>
+                </td>
+			</tr>
+            <tr>
+				<th class="bg-danger text-right">ความต้องการอาชีพ</th>
+				<td>
+                    <ul class="list-group">
+                        <li class="list-group-item">เกษตร<span class="pull-right"><span class="badge bg-red"><?=$require_household->count_avocation_by_district_id('AVOCATION_FARM',$project_planning->DISTRICT_ID)?></span> ครัวเรือน</span></li>
+                        <li class="list-group-item">เลี้ยงสัตว์<span class="pull-right"><span class="badge bg-red"><?=$require_household->count_avocation_by_district_id('AVOCATION_ANIMAL',$project_planning->DISTRICT_ID)?></span> ครัวเรือน</span></li>
+                        <li class="list-group-item">ประมง<span class="pull-right"><span class="badge bg-red"><?=$require_household->count_avocation_by_district_id('AVOCATION_FISHER',$project_planning->DISTRICT_ID)?></span> ครัวเรือน</span></li>
+                        <li class="list-group-item">ช่างฝีมือ/คหกรรม<span class="pull-right"><span class="badge bg-red"><?=$require_household->count_avocation_by_district_id('AVOCATION_TECH',$project_planning->DISTRICT_ID)?></span> ครัวเรือน</span></li>
+                        <li class="list-group-item">ค้าขาย<span class="pull-right"><span class="badge bg-red"><?=$require_household->count_avocation_by_district_id('AVOCATION_TRADE',$project_planning->DISTRICT_ID)?></span> ครัวเรือน</span></li>
+                        <li class="list-group-item">ทุนประกอบอาชีพ<span class="pull-right"><span class="badge bg-red"><?=$require_household->count_avocation_by_district_id('AVOCATION_CAREER',$project_planning->DISTRICT_ID)?></span> ครัวเรือน</span></li>
+                        <li class="list-group-item">ทุนการศึกษา<span class="pull-right"><span class="badge bg-red"><?=$require_household->count_avocation_by_district_id('AVOCATION_EDUCATION',$project_planning->DISTRICT_ID)?></span> ครัวเรือน</span></li>
+                        <li class="list-group-item">อื่นๆ<span class="pull-right"><span class="badge bg-red"><?=$require_household->count_avocation_by_district_id('AVOCATION_ETC',$project_planning->DISTRICT_ID)?></span> ครัวเรือน</span></li>
+                    </ul>
+                </td>
+			</tr            
                 </tbody>
                 </table>
         </div>
