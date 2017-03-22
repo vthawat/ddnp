37377522d7d83a50a9d2dee14f5b738ecd07834c
vthawat <thawat.va@email.psu.ac.th>
2017-03-23 06:52:43 +0700

    config tiny

--- filemanager/config/config.php
+++ filemanager/config/config.php
@@ -58,7 +58,7 @@ $config = array(
 	| without final / (DON'T TOUCH)
 	|
 	*/
-	'base_url' => ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && ! in_array(strtolower($_SERVER['HTTPS']), array( 'off', 'no' ))) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'].'/ridm',
+	'base_url' => ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && ! in_array(strtolower($_SERVER['HTTPS']), array( 'off', 'no' ))) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'].'/ddnp',
 	//'base_url'=>'/ridm/',
 	/*
 	|--------------------------------------------------------------------------
